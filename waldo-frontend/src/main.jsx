aff6ef6ca695d938ccae1c21c05423c775cc6bf8
jdogcodey <jonnydawson@hotmail.com>
2025-08-01 21:09:54 +0100

    chore: set up outline for routes

--- waldo-frontend/src/main.jsx
+++ waldo-frontend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom' 
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root />} errorElement={<ErrorPage />}>
+
+  </Route>
+))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
