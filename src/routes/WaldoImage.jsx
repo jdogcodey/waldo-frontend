6d2461c760f70ad3296bf9cba8a6abbb5f66109e
jdogcodey <jonnydawson@hotmail.com>
2025-08-03 20:17:52 +0100

    feat: changed x and y to a 0 to 1 scale to accommodate other img sizes

--- src/routes/WaldoImage.jsx
+++ src/routes/WaldoImage.jsx
@@ -1,32 +1,41 @@
+import { useState, useEffect } from 'react';
 import waldoImage from '../assets/wp7156925-wheres-wally-wallpapers.jpg'
+import { useAppContext } from '../contexts/AppContext';
 
 export default function WaldoImage() {
+    const { username, sessionID } = useAppContext();
+    const [sessionIDReturned, setSessionIDReturned] = useState(false)
 
+    useEffect(() => {
+        if (sessionID && username) {
+            setSessionIDReturned(true)
+        }
+    }, [sessionID, username])
 
-    function clickImg(e) {
+    async function clickImg(e) {
         let imgRect = e.target.getBoundingClientRect();
-        let x = e.clientX - imgRect.left;
-        let y = e.clientY - imgRect.top;
+        let x = (e.clientX - imgRect.left) /imgRect.width;
+        let y = (e.clientY - imgRect.top) /imgRect.height;
         console.log(x)
         console.log(y)
-        // const baseURL = import.meta.env.VITE_API_BASE_URL;
-        // const response = await fetch(`${baseURL}/endgame`, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: {
-        //         x: x,
-        //         y: y,
-        //         id: id,
-        //         username: username,
-        //     }
-        // })
+        const baseURL = import.meta.env.VITE_API_BASE_URL;
+        await fetch(`${baseURL}/endgame`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                x: x,
+                y: y,
+                id: sessionID,
+                username: username,
+            }
+        })
     }
     return (
         <main>
             <h1>Click on Waldo</h1>
-            <img src={waldoImage} alt="Where's Waldo... I can't give you clues" onClick={e => clickImg(e)}/>
+            <img src={waldoImage} alt="Where's Waldo... I can't give you clues" onClick={e => clickImg(e)} style={{pointerEvents: sessionIDReturned ? 'auto' : 'none'}}/>
         </main>
         
     )
